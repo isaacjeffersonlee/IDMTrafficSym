ddeb114daca913b5075a69d06e9b108ef7e5718a
isaacjeffersonlee <27ilee01@gmail.com>
2022-06-13 17:42:43 +0100

    add common.h

---
+++ includes/common.h
@@ -0,0 +1,5 @@
+extern float fps;    // Frames per second
+extern float dt;  // Time between frames
+extern bool export_data;
+extern float simTime; // Simulation time in seconds
+extern int totalFrameNum;  // Total number of frames
